288449dcb22f295d91dc5aca84efc64b776091c4
joaomanita <joaomanoita3@gmail.com>
2026-03-02 10:40:59 +0000

    add print_exp function

--- lib/synthesizer.ml
+++ lib/synthesizer.ml
@@ -12,23 +12,23 @@ let fresh_id =
 type tm =
   | Var of id
   | Offer of label * tm
-  | Case of tm * tm list
-  | OfferChoice of tm list
-  | Choose of (tm * label) * tm
+  | Case of id * (label * tm) list
+  | OfferChoice of (label * tm) list
+  | Choose of (id * label) * tm
   | SendChannelFrom of tm * tm
-  | ReceiveChannelFrom of (tm * tm) * tm
+  | ReceiveChannelFrom of id * tm
   | ReceiveChannel of tm
-  | SendChannelTo of (tm * tm) * tm
+  | SendChannelTo of (id * tm) * tm
   | SendValue of tm * tm
-  | SendValueTo of (tm * tm) * tm
-  | ReceiveValueFrom of (tm * tm) * tm
+  | SendValueTo of (id * tm) * tm
+  | ReceiveValueFrom of id * tm
   | ReceiveValue of tm
   | Terminate
-  | Wait of tm * tm
+  | Wait of id * tm
   | Detach of tm
-  | Release of tm * tm
+  | Release of id * tm
   | Accept of tm
-  | Accquire of tm * tm
+  | Accquire of id * tm
 
 type index = Z | S of index
 type lens = index
@@ -45,12 +45,54 @@ and print_type t =
   | TyPrimitive t -> t
   | TyInternalChoice l -> "InternalChoice<" ^ print_hlist l ^ ">"
   | TyExternalChoice l -> "ExternalChoice<" ^ print_hlist l ^ ">"
-  | TySendChannel t ->
-      "SendChannel<" ^ print_type (fst t) ^ print_type (snd t) ^ ">"
-  | TyReceiveChannel t ->
-      "ReceiveChannel<" ^ print_type (fst t) ^ print_type (snd t) ^ ">"
-  | TySendValue t -> "SendValue<" ^ fst t ^ print_type (snd t) ^ ">"
-  | TyReceiveValue t -> "ReceiveValue<" ^ fst t ^ print_type (snd t) ^ ">"
+  | TySendChannel (t1, t2) ->
+      "SendChannel<" ^ print_type t1 ^ print_type t2 ^ ">"
+  | TyReceiveChannel (t1, t2) ->
+      "ReceiveChannel<" ^ print_type t1 ^ print_type t2 ^ ">"
+  | TySendValue (t1, t2) -> "SendValue<" ^ t1 ^ print_type t2 ^ ">"
+  | TyReceiveValue (t1, t2) -> "ReceiveValue<" ^ t1 ^ print_type t2 ^ ">"
   | TyEnd -> "End"
   | TySharedToLinear t -> "SharedToLinear<" ^ print_type t ^ ">"
   | TyLinearToShared t -> "LinearToShared<" ^ print_type t ^ ">"
+
+open Printf
+
+let rec print_exp e =
+  match e with
+  | Var a -> a
+  | Offer (label, tm) -> sprintf "offer_case!(%s, %s)" label (print_exp tm)
+  | Case (chan, choices) ->
+      sprintf "case!{ %s ; %s }" chan (print_labeled_choices choices)
+  | OfferChoice choices ->
+      sprintf "offer_choice!{ %s }" (print_labeled_choices choices)
+  | Choose ((chan, label), tm) ->
+      sprintf "choose!(%s, %s, %s)" chan label (print_exp tm)
+  | SendChannelFrom (chan_exp, tm) ->
+      sprintf "send_channel_from(%s, %s)" (print_exp chan_exp) (print_exp tm)
+  | ReceiveChannelFrom (chan, tm) ->
+      sprintf "receive_channel_from(%s, %s)" chan (print_exp tm)
+  | ReceiveChannel tm -> sprintf "receive_channel(%s)" (print_exp tm)
+  | SendChannelTo ((chan, chan_exp), tm) ->
+      sprintf "send_channel_to(%s, %s, %s)" chan (print_exp chan_exp)
+        (print_exp tm)
+  | SendValue (v, tm) ->
+      sprintf "send_value(%s, %s)" (print_exp v) (print_exp tm)
+  | SendValueTo ((chan, v), tm) ->
+      sprintf "send_value_to(%s, %s, %s)" chan (print_exp v) (print_exp tm)
+  | ReceiveValueFrom (chan, tm) ->
+      sprintf "receive_value_from(%s, %s)" chan (print_exp tm)
+  | ReceiveValue tm -> sprintf "receive_value(%s)" (print_exp tm)
+  | Terminate -> "terminate ()"
+  | Wait (chan, tm) -> sprintf "wait(%s, %s)" chan (print_exp tm)
+  | Detach tm -> sprintf "detach_shared_session(%s)" (print_exp tm)
+  | Release (chan, tm) ->
+      sprintf "release_shared_session(%s, %s)" chan (print_exp tm)
+  | Accept tm -> sprintf "accept_shared_session(%s)" (print_exp tm)
+  | Accquire (chan, tm) ->
+      sprintf "acquire_shared_session(%s, %s)" chan (print_exp tm)
+
+and print_labeled_choices l =
+  match l with
+  | [] -> ""
+  | (label, tm) :: t ->
+      label ^ " => " ^ print_exp tm ^ " " ^ print_labeled_choices t
